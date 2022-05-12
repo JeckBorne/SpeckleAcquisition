4e1be36f596fac15546bcf7f7648739fbc31177c
jorou125 <jorou125@ulaval.ca>
2022-05-12 15:58:39 -0400

    HistogramFonctionality
    This commit is to add the feature to create an histogram independently of Fiji to analyse the distribution of pixel intensity.

--- CameraControl.py
+++ CameraControl.py
@@ -1,11 +1,12 @@
 # This code is to save a single frame of video feed
-# for analysis
+# for analysis and creates an histogram of pixel intensity
 
 import cv2
+import matplotlib.pyplot as plt
 
 
 # Sets up a VideoCapture object
-video_feed = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+video_feed = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 # Checks if the camera is accesible
 if not video_feed.isOpened():
     raise IOError("Cannot connect to camera")
@@ -21,6 +22,9 @@ while(True):
             relative_path = f"Figure/{filename}.png" # Where the image will be saved and what format
             cv2.imwrite(relative_path, frame) # Saves frame
             print(f"The image was successfully saved as {relative_path}")
+            plt.hist(frame.flatten(), bins=256, range=(0, 255)) # Creates histogram of pixel intensity
+            plt.savefig(f"Figure/{filename}Histogram.png") # Saves the histogram
+            plt.show() # Shows the histogram
             break
         if cv2.waitKey(1) & 0xFF == ord("q"): # Press "q" to stop video feed
             break
